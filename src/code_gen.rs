28ea347ee65df83e964aea4001a27aa05673aca3
Olvior <olvioro@gmail.com>
2024-10-02 07:47:07 +0200

    Changed the way code gen works

--- src/code_gen.rs
+++ src/code_gen.rs
@@ -1,12 +1,22 @@
-use crate::parser::NodeProgram;
-
+use crate::parser::*;
 
 pub fn code_gen(ast: &NodeProgram) -> String {
     let mut asm = String::from("global _start\n_start:\n");
     
-    asm.push_str("    mov rax, 60\n");
-    //asm.push_str(&format!("    mov rdi, {}\n", ast.expr.int_lit.info));
-    asm.push_str("    syscall\n");
+    for stmt in &ast.statements {
+        match stmt {
+            NodeStatements::Declare(_declare_stmt) => {
+            },
+            NodeStatements::Exit(exit_stmt) => {
+                let int_lit = &exit_stmt.expression.int_lit;
+                asm.push_str("    mov rax, 60\n");
+                asm.push_str(&format!("    mov rdi, {}\n", int_lit.info));
+                asm.push_str("    syscall\n");
+
+            }
+            //_ => { println!("Unable to generate code for {:?}", stmt) }
+        }
+    }
 
     return asm
 }
