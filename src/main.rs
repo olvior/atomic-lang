bfa22e97bf10aa668182056fd47b207f0f92f07d
Olvior <olvioro@gmail.com>
2024-10-12 10:46:30 +0200

    Improved main with better command line handling

--- src/main.rs
+++ src/main.rs
@@ -13,60 +13,189 @@ use parser::Parser;
 mod code_gen;
 use code_gen::CodeGen;
 
-fn main() {
+#[derive(PartialEq)]
+enum Options {
+    // delete the asm and object files
+    Clean,
+}
+
+struct Settings {
+    f_in: String,
+    f_out: String,
+    options: Vec<Options>,
+}
+
+fn collect_settings() -> Settings {
     let args: Vec<String> = env::args().collect();
-    // if args.len() < 3 {
-    //     println!("Useage: ./lithium path/to/file.li path/to/out");
-    //     return;
-    // }
 
-    // let file_path = &args[1];
-    // let out_path = &args[2];
-    let file_path = "test.li";
-    let out_path = "bin/test";
+    let mut options: Vec<Options> = vec!();
+    let mut arguments: Vec<String> = vec!();
+
+    // loop but ignore first arg
+    for arg in args[1..].into_iter() {
+        // if its an option
+        if arg.chars().nth(0).expect("Arg was empty") == '-' {
+            for c in arg[1..].chars().into_iter() {
+
+                options.push(
+                    match c {
+                        // clean, delete other files
+                        'c' => Options::Clean,
+
+                        _ => panic!("Unknown option {}", c),
+                    }
+                );
+
+            }
+        }
+
+        // if its just an argument
+        else {
+            arguments.push(arg.to_string());
+        }
+    }
+
+    if arguments.len() < 1 {
+        exit_message("Useage: ./lithium path/to/file.li");
+    }
+
+    dbg!(&arguments);
+    let f_in = arguments[0].clone();
+
+    let f_out: String;
+
+    if arguments.len() > 1 {
+        f_out = arguments[1].clone();
+    } else {
+        f_out = format!("bin/{}", &f_in[0..f_in.len() - 3]);
+        dbg!(&f_out);
+    }
+
+    let settings = Settings { f_in, f_out, options };
+
+    return settings;
+}
+
+fn read_in(settings: &Settings) -> String {
+    let path = Path::new(&settings.f_in);
     
-    let path = Path::new(file_path);
+    let Ok(mut file) = File::open(path) else { panic!("Could not find {}", &settings.f_in); };
 
-    let Ok(mut file) = File::open(path) else { println!("Could not find {}", file_path); return; };
+    let mut contents = String::new();
 
-    let mut s = String::new();
-    if let Err(err) = file.read_to_string(&mut s) { println!("Could not read {} due to {}", file_path, err); return; }
+    if let Err(err) = file.read_to_string(&mut contents) { panic!("Could not read {} due to {}", &settings.f_in, err); }
 
-    let mut tokeniser = Tokeniser{ source: s, index: 0};
-    let tokenised = tokeniser.tokenise();
-    dbg!(&tokenised);
-    let mut parser = Parser { tokens: tokenised, index: 0};
-    let parse_tree = parser.parse();
-    dbg!(&parse_tree);
+    return contents;
+}
 
-    let mut generator = CodeGen::new();
-    generator.generate(&parse_tree);
-    let asm = generator.asm;
-    print!("{}", asm);
+fn write_out(settings: &Settings, asm: &str) {
+    let path = &format!("{}.asm", &settings.f_out);
 
-    let path = &format!("{}.asm", out_path);
     let Ok(mut output_file) = File::create(path) else {
         println!("Could not create output file!");
         return;
     };
 
     write!(output_file, "{}", asm).expect("Could not write output asm file!");
+}
 
-    let _ = Command::new("sh")
+fn assemble(settings: &Settings) {
+    let out_path = &settings.f_out;
+
+    let nasm_output = Command::new("sh")
         .arg("-c")
         .arg(&format!("nasm -felf64 {}.asm -o {}.o", out_path, out_path))
         .output();
 
-    //dbg!(a);
-    
-    
-    let _ = Command::new("sh")
+    if nasm_output.is_ok() {
+        println!("Assembled");
+    } else {
+        println!("Did not assemble:\n{}", nasm_output.unwrap_err());
+    }
+}
+
+fn link(settings: &Settings) {
+    let out_path = &settings.f_out;
+
+    let linker_output = Command::new("sh")
         .arg("-c")
         .arg(&format!("ld -o {} {}.o", out_path, out_path))
         .output();
+
+    if linker_output.is_ok() {
+        println!("Linked");
+    } else {
+        println!("Did not link:\n{}", linker_output.unwrap_err());
+    }
+}
+
+fn clean_asm(settings: &Settings) {
+    let out_path = &settings.f_out;
+
+    let rm_asm_output = Command::new("sh")
+        .arg("-c")
+        .arg(&format!("rm {}.asm", out_path))
+        .output();
+    
+    if rm_asm_output.is_ok() {
+        println!("Cleaned asm file");
+    } else {
+        println!("Did not clean asm file:\n{}", rm_asm_output.unwrap_err());
+    }
+}
+
+fn clean_object(settings: &Settings) {
+    let out_path = &settings.f_out;
+
+    let rm_obj_output = Command::new("sh")
+        .arg("-c")
+        .arg(&format!("rm {}.o", out_path))
+        .output();
+
+    if rm_obj_output.is_ok() {
+        println!("Cleaned object file");
+    } else {
+        println!("Did not clean obj:\n{}", rm_obj_output.unwrap_err());
+    }
+}
+
+fn clean_files(settings: &Settings) {
+    clean_asm(settings);
+    clean_object(settings);
+}
+
+fn main() {
+    let settings = collect_settings();
+
+    let source_code = read_in(&settings);
+
+    let mut tokeniser = Tokeniser{ source: source_code, index: 0};
+    let tokenised = tokeniser.tokenise();
+    dbg!(&tokenised);
+
+    let mut parser = Parser { tokens: tokenised, index: 0};
+    let parse_tree = parser.parse();
+    dbg!(&parse_tree);
+
+    let mut generator = CodeGen::new();
+    generator.generate(&parse_tree);
+    let asm = &generator.asm;
+    print!("{}", asm);
+
+    write_out(&settings, asm);
+
+
+    assemble(&settings);
+
+    link(&settings);
+
+    if settings.options.contains(&Options::Clean) {
+        clean_files(&settings);
+    }
 }
 
 fn exit_message(message: &str) {
     println!("{}", message);
     std::process::exit(127)
 }
+
